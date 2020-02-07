add91a7e880597241ee0309f0db618bcb67ef9dd
Susanne Werner-Most <susanne.werner.most@gmail.com>
2020-02-07 22:43:10 +0100

    add firebase authentication

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,26 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import * as firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+const configOptions = {
+  apiKey: "AIzaSyCXV3PTVK72fQIarjtuiJ7HtQ-09ni0T74",
+  authDomain: "weliveto.firebaseapp.com",
+  databaseURL: "https://weliveto.firebaseio.com",
+  projectId: "weliveto",
+  storageBucket: "weliveto.appspot.com",
+  messagingSenderId: "818077967920",
+  appId: "1:818077967920:web:7e667bb10f13896675aff5",
+  measurementId: "G-WX132PYNGR"
+};
+
+firebase.initializeApp(configOptions);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
 
 new Vue({
   router,
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,46 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../components/Login.vue";
+import Register from "../components/Register.vue";
+import Dashboard from "../components/Dashboard.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
 const router = new VueRouter({
   mode: "history",
-  routes
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "About",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/About.vue")
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: Register
+    },
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      component: Dashboard
+    }
+  ]
 });
 
 export default router;
